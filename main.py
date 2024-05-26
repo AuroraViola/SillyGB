f4eef9bedceb1c5e86e20279280a20ac4301912d
AuroraViola <110638310+AuroraViola@users.noreply.github.com>
2024-05-27 00:42:48 +0200

    rewrite of ppu

--- main.py
+++ main.py
@@ -7,40 +7,14 @@ import cartridge
 import joypad
 
 
-def createSquare(x, y, color):
-    pygame.draw.rect(pygamedisplay, color, [x, y, size_mul, size_mul])
-def visualizeGrid(display):
-    y = 0
-    for row in display:
-        x = 0
-        for item in row:
-            if item == 0:
-                createSquare(x, y, (185, 237, 186))
-            elif item == 1:
-                createSquare(x, y, (118, 196, 123))
-            elif item == 2:
-                createSquare(x, y, (49, 105, 64))
-            else:
-                createSquare(x, y, (10, 38, 16))
-
-            x += size_mul
-        y += size_mul
-
-    pygame.display.update()
-
-
-size_mul = 3
+scaling_fact = 4
 
 if __name__ == "__main__":
     # Initialize display
-    pygame.init()
-    pygamedisplay = pygame.display.set_mode((160 * size_mul, 144 * size_mul))
-    pygame.display.set_caption("SillyGB")
-    display = ppu.Display()
-    cpu.memory[0xff44] = 0x90
+    screen = ppu.Display(4, cpu.memory)
 
     # Load cartridge to memory
-    cartridge = cartridge.Cartridge("Roms/Private/SillyTest.gb")
+    cartridge = cartridge.Cartridge("Roms/Private/tellinglys.gb")
     cartridge.load_rom(cpu.memory)
 
     # Initialize joypad
@@ -50,29 +24,29 @@ if __name__ == "__main__":
     tick = 1
     done = False
     while not done:
-        eventlist = pygame.event.get()
-        for ev in eventlist:
-            if ev.type == pygame.QUIT:
-                done = True
-            if ev.type == pygame.KEYDOWN:
-                cpu.memory[0xff0f] |= 16
-                for button in p1.buttonskeys:
-                    if ev.key == p1.buttonskeys[button]:
-                        cpu.memory[0xff0f] |= 16
-                        p1.buttons[button] = 1
-            if ev.type == pygame.KEYUP:
-                for button in p1.buttonskeys:
-                    if ev.key == p1.buttonskeys[button]:
-                        p1.buttons[button] = 0
+        if tick % 18240 == 0:
+            #screen.visualize_display(cpu.memory)
+            eventlist = pygame.event.get()
+            for ev in eventlist:
+                if ev.type == pygame.QUIT:
+                    done = True
+                if ev.type == pygame.KEYDOWN:
+                    for button in p1.buttonskeys:
+                        if ev.key == p1.buttonskeys[button]:
+                            cpu.memory[0xff0f] |= 16
+                            p1.buttons[button] = 1
+                if ev.type == pygame.KEYUP:
+                    for button in p1.buttonskeys:
+                        if ev.key == p1.buttonskeys[button]:
+                            p1.buttons[button] = 0
+            print(cpu.clock.t_states)
+            cpu.memory[0xff00] = p1.encode_buttons(cpu.memory[0xff00])
+            cpu.memory[0xff00] = p1.encode_buttons(cpu.memory[0xff00])
 
-        prev_btn = cpu.memory[0xff00]
         cpu.memory[0xff00] = p1.encode_buttons(cpu.memory[0xff00])
 
         cpu.execute()
 
-        if tick % 18240 == 0:
-            display.load_background(cpu.memory)
-            visualizeGrid(display.background)
         tick += 1
 
     pygame.quit()
--- ppu.py
+++ ppu.py
@@ -1,8 +1,39 @@
+import pygame
 import cpu
 
 class Display:
+    display = [[0 for _ in range(160)] for _ in range(144)]
     background = [[0 for _ in range(256)] for _ in range(256)]
     window = [[0 for _ in range(256)] for _ in range(256)]
+    sprites = []
+    palette = (
+        (185, 237, 186),
+        (118, 196, 123),
+        (49, 105, 64),
+        (10, 38, 16)
+    )
+
+    def __init__(self, scaling_fact, memory):
+        pygame.init()
+        self.scaling_fact = scaling_fact
+        self.win = pygame.display.set_mode((160 * scaling_fact, 144 * scaling_fact))
+        self.screen = pygame.Surface((160, 144))
+        pygame.display.set_caption("SillyGB")
+        memory[0xff44] = 0x90
+
+    def visualize_display(self, memory):
+        self.load_display(memory)
+
+        y = 0
+        for i in range(144):
+            x = 0
+            for j in range(160):
+                pygame.draw.rect(self.screen, self.palette[self.display[i][j]], [x, y, 1, 1])
+                x += 1
+            y += 1
+
+        self.win.blit(pygame.transform.scale(self.screen, self.win.get_rect().size), (0, 0))
+        pygame.display.update()
 
     def decode_tile(self, memory: cpu.Memory, addr: int):
         tile = []
@@ -26,7 +57,7 @@ class Display:
 
     def get_tiles(self, memory: cpu.Memory):
         tiles = []
-        if ((memory[0xff40] & 16) >> 4) == 1:
+        if (memory[0xff40] & 16) != 0:
             i = 0x8000
             while i < 0x8800:
                 tiles.append(self.decode_tile(memory, i))
@@ -43,16 +74,37 @@ class Display:
 
         return tiles
 
+
     def load_background(self, memory: cpu.Memory):
-        self.reset_background()
+            tiles = self.get_tiles(memory)
+            bg_tiles = []
+            if (memory[0xff40] & 8) == 0:
+                for i in range(0x9800, 0x9c00):
+                    bg_tiles.append(tiles[memory[i]])
+            else:
+                for i in range(0x9c00, 0xa000):
+                    bg_tiles.append(tiles[memory[i]])
+
+            ti = 0
+            for y in range(32):
+                for x in range(32):
+                    display_x = x * 8
+                    display_y = y * 8
+
+                    for y1 in range(8):
+                        for x1 in range(8):
+                            self.background[display_y + y1][display_x + x1] = bg_tiles[ti][y1][x1]
+                    ti += 1
+
+    def load_window(self, memory: cpu.Memory):
         tiles = self.get_tiles(memory)
-        bg_tiles = []
-        if ((memory[0xff40] & 8) >> 3) == 0:
+        win_tiles = []
+        if (memory[0xff40] & 64) == 0:
             for i in range(0x9800, 0x9c00):
-                bg_tiles.append(tiles[memory[i]])
+                win_tiles.append(tiles[memory[i]])
         else:
             for i in range(0x9c00, 0xa000):
-                bg_tiles.append(tiles[memory[i]])
+                win_tiles.append(tiles[memory[i]])
 
         ti = 0
         for y in range(32):
@@ -62,11 +114,31 @@ class Display:
 
                 for y1 in range(8):
                     for x1 in range(8):
-                        self.background[display_y + y1][display_x + x1] = bg_tiles[ti][y1][x1]
+                        self.window[display_y + y1][display_x + x1] = win_tiles[ti][y1][x1]
                 ti += 1
 
+    def load_sprite(self, memory):
+        self.sprite = []
+        i = 0xfe00
+        while i < 0xfea0:
+            ypos = memory[i]
+            xpos = memory[i + 1]
+            t_index = memory[i + 2]
+            attributes = memory[i + 3]
+
+            priority = attributes >> 7
+            yflip = (attributes >> 6) & 1
+            xflip = (attributes >> 5) & 1
+            self.sprites.append((ypos, xpos, t_index, priority, yflip, xflip))
+            i += 4
+
     def reset_background(self):
-        self.background = [[0 for _ in range(256)] for _ in range(256)]
+        self.background = [[0 for _ in range(512)] for _ in range(512)]
 
     def reset_window(self):
         self.window = [[0 for _ in range(256)] for _ in range(256)]
+
+    def load_display(self, memory: cpu.Memory()):
+        self.reset_background()
+        if memory[0xff40] & 1 != 0:
+            self.load_background(memory)
